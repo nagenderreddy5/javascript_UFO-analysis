418b4adb70afa96dddb894d2f129ee256b9242d9
nagenderreddy5 <69405759+nagenderreddy5@users.noreply.github.com>
2020-12-22 20:52:21 -0500

    app.js

---
+++ app.js
@@ -0,0 +1,49 @@
+// from data.js
+var observations = data;
+
+var tbody = d3.select("tbody");
+
+// ***************************************************
+
+observations.forEach(function(ufoInfo) {
+    console.log(ufoInfo);
+    var row = tbody.append("tr");
+
+// update each cell's values with ufo info using d3.
+    Object.entries(ufoInfo).forEach(function([key, value]){
+        console.log(key, value);
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
+
+// ********************************************************
+
+var submit = d3.select("#filter-btn");
+var empty = d3.select("tbody")
+submit.on("click", function() {
+    empty.html("")
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputElement = d3.select("#state");
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(observations);
+
+    var filteredData = observations.filter(bydate => bydate.datetime === inputValue);
+
+    console.log(filteredData);
+
+    filteredData.forEach(function(filteredReport) {
+        console.log(filteredReport);
+        var row = tbody.append("tr");
+    
+    // update each cell's values with ufo info using d3.
+        Object.entries(filteredReport).forEach(function([key, value]){
+            console.log(key, value);
+            var cell = tbody.append("td");
+            cell.text(value);
+        });
+    });
+});
